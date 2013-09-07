c52a8b092a6214028eba1239ac2f658d0d665a4b
futur24 <velerofont@agnostosx-laptop.lan>
2013-09-07 17:23:32 +0300

    left menu search created, only for user

--- Resources/ui/iphone/menu_left.js
+++ Resources/ui/iphone/menu_left.js
@@ -17,8 +17,20 @@ var winLeft = Ti.UI.createWindow();
 var leftmenuSearchView = Ti.UI.createView({
 	backgroundColor:UI_MENU_BACKGROUND_COLOR,
 	height:40,
-	top:0
+	top:0,
+	zIndex:5
 });
+
+//close button for search - default hidden
+var menuLeftCloseSearchViewButton = Titanium.UI.createButton({
+	title:'close',
+	right:9,
+	width:50,
+	height:29
+});
+leftmenuSearchView.add(menuLeftCloseSearchViewButton);
+menuLeftCloseSearchViewButton.addEventListener('click', handleLeftCloseSearchButton);
+menuLeftCloseSearchViewButton.hide();
 
 var leftmenuSearchContainer = Titanium.UI.createImageView({
 	image:IMAGE_PATH+'menu_left/search_field.png',
@@ -33,11 +45,13 @@ var leftmenuSearchIcon = Titanium.UI.createImageView({
 var leftmenuSearchTxtfield = Titanium.UI.createTextField({
 	left:35,
 	width:200,
-	field:'search'
+	field:'search',
+	returnKeyType: Ti.UI.RETURNKEY_SEARCH
 });
 
 leftmenuSearchContainer.add(leftmenuSearchTxtfield);
-leftmenuSearchTxtfield.addEventListener('change', handleLeftMenuTextFieldFocus);
+leftmenuSearchTxtfield.addEventListener('change', handleLeftMenuTextFieldChange);
+leftmenuSearchTxtfield.addEventListener('focus', handleLeftMenuTextFieldFocus);
 leftmenuSearchTxtfield.addEventListener('blur', handleLeftMenuTextFieldBlur);
 
 var leftmenuSearchTxtfieldLabel = Titanium.UI.createLabel({
@@ -59,6 +73,37 @@ winLeft.add(leftmenuSearchView);
 var leftTableView = createLeftMenu();
 
 winLeft.add(leftTableView);
+
+//background for search results
+var leftmenuSearchBackgroundView = Ti.UI.createView({
+	backgroundColor:'black',
+	opacity:0.5
+});
+
+winLeft.add(leftmenuSearchBackgroundView);
+leftmenuSearchBackgroundView.hide();
+
+//activity indicator for search
+var leftmenuSearchActivityIndicator = Titanium.UI.createActivityIndicator({
+	top:50,
+    style:Titanium.UI.iPhone.ActivityIndicatorStyle.PLAIN
+});
+winLeft.add(leftmenuSearchActivityIndicator);
+
+//table view for search results
+var menuLeftSearchResultsTableView = Titanium.UI.createTableView({
+	backgroundColor:UI_MENU_BACKGROUND_COLOR,
+	font:UI_FONT_LEFTMENU,
+	top:40
+});
+menuLeftSearchResultsTableView.addEventListener('click', handleLeftSearchResultRows);
+
+//remove empty rows
+menuLeftSearchResultsTableView.footerView = Ti.UI.createView({
+    height: 1,
+    backgroundColor: 'transparent'
+});
+leftmenuSearchBackgroundView.add(menuLeftSearchResultsTableView);
 
 leftTableView.addEventListener("click", function(e){
 	Ti.API.info("isAnyViewOpen: " + window.isAnyViewOpen());
@@ -318,10 +363,52 @@ function createLeftMenuRow(menuItem){
 	return row;
 }
 
-function handleLeftMenuTextFieldFocus(e){
-	var field = e.source.field;
+//populate search result rows - primarily for users only
+function populateSearchResultsTableView(uObj){
+	
+	var tableRows = [];
+	
+	for(i=0;i<uObj.length;i++){
 		
+		var row = Ti.UI.createTableViewRow({
+			height:49,
+			className:'searchResult',
+			backgroundColor:UI_MENU_BACKGROUND_COLOR,
+			selectedBackgroundColor:'#1c2027',
+			user_id:uObj[i].User.id
+		});
+		
+		var rowNameLabel = Titanium.UI.createLabel({
+			text:uObj[i].User.name,
+			color:'#ab7b04',
+			left:52,
+			font:{fontSize:18, fontWeight:'regular', fontFamily:'Open Sans'}
+		});
+		
+		row.add(rowNameLabel);
+		tableRows.push(row);
+	}
+	
+	menuLeftSearchResultsTableView.setData(tableRows);
+}
+
+//handle text field change
+function handleLeftMenuTextFieldChange(e){
+	var field = e.source.field;
+	var name = e.source.value;	
+	
 	if(field == 'search'){
+		//on textfield change, change the opacity and get users found
+		if(name != ''){
+			leftmenuSearchBackgroundView.opacity = 1;
+			getLeftMenuOnlineUser(name);
+		}else{
+			//if textfield empty, reset search view
+			leftmenuSearchBackgroundView.opacity = 0.5;
+			leftmenuSearchBackgroundView.backgroundColor = 'black';
+			menuLeftSearchResultsTableView.data = [];
+		}
+		
 		if(leftmenuSearchTxtfield.value != ''){
 			leftmenuSearchTxtfieldLabel.hide();
 		}else{
@@ -329,14 +416,100 @@ function handleLeftMenuTextFieldFocus(e){
 		}
 	}
 }
+
+//handle textfield focus
+function handleLeftMenuTextFieldFocus(e){
+	if(menuLeftSearchResultsTableView.data == []){
+		
+	}
+	leftmenuSearchBackgroundView.show();
+	window.leftLedge = 0;
+	window.setParallaxAmount(0.0);
+	menuLeftCloseSearchViewButton.show();
+}
+
+//handle close button for search
+function handleLeftCloseSearchButton(){
+	clearSearchBackground();
+	window.leftLedge = 65;
+	window.setParallaxAmount(0.3);
+}
 	
 //handle textfield when not focused
 function handleLeftMenuTextFieldBlur(e){
 	var field = e.source.field;
 	
 	if(field == 'search'){
+		
 		if(leftmenuSearchTxtfield.value == ''){
 			leftmenuSearchTxtfieldLabel.show();
 		}
 	}
+}
+
+//get online users
+function getLeftMenuOnlineUser(n){
+	Ti.API.info('getLeftMenuOnlineUser() called for user='+ n);
+	leftmenuSearchBackgroundView.backgroundColor = UI_MENU_BACKGROUND_COLOR;
+	leftmenuSearchActivityIndicator.show();
+	
+	var xhr = Ti.Network.createHTTPClient();
+	xhr.setTimeout(NETWORK_TIMEOUT);
+	
+	xhr.onerror = function(e){
+	
+	};
+	
+	xhr.onload = function(e) {
+		var jsonData = JSON.parse(this.responseText);
+		
+		if (jsonData.data.response == NETWORK_RESPONSE_OK){
+			leftmenuSearchActivityIndicator.hide();
+			populateSearchResultsTableView(jsonData.data.users);
+		}else{
+			alert(getErrorMessage(jsonData.response));
+		}
+		
+	};
+	xhr.open('GET',API+'searchUser');
+	xhr.send({
+		name:n
+	});
+}
+
+//handle user rows from search 
+function handleLeftSearchResultRows(e){
+	clearSearchBackground();
+	
+	closeOpenWindows();
+	
+	var userId = e.row.user_id;
+	//show profile view if the user, is the device owner, else show profile other view
+	if (userId == userObject.userId){
+		Ti.include('ui/iphone/profile.js');
+		navController.getWindow().add(viewProfile);
+		navController.getWindow().setTitle(userObject.name);
+	}else{
+		Ti.include('ui/iphone/profile_other.js');
+		
+		var profileOtherView = buildProfileOtherView(userId);
+		var nameUser = e.row.children[0].text;
+		
+		navController.getWindow().add(profileOtherView);
+		navController.getWindow().setTitle(nameUser);
+	}
+	if(window.isAnyViewOpen()){
+		window.toggleLeftView();
+	}
+	
+	window.leftLedge = 65;
+	window.setParallaxAmount(0.3);
+}
+
+//change properties and data back to default
+function clearSearchBackground(){
+	leftmenuSearchActivityIndicator.hide();
+	leftmenuSearchBackgroundView.hide();
+	menuLeftCloseSearchViewButton.hide();
+	leftmenuSearchTxtfield.blur();
 }
