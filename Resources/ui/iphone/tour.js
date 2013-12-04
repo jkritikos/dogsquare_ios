c4f1a02c4bc5c4885017eccedd1710328cd1cb87
jkritikos <jkritikos@gmail.com>
2013-12-04 13:28:42 +0200

    new tour images

--- Resources/ui/iphone/tour.js
+++ Resources/ui/iphone/tour.js
@@ -35,15 +35,16 @@ function openTourView(){
 	});
 	
 	//tour content
-	var view1 = Ti.UI.createImageView({backgroundColor:'white',image:IPHONE5? IMAGE_PATH+'tour/12-568h@2x.png' : IMAGE_PATH+'tour/12.png' });
-	var view2 = Ti.UI.createImageView({backgroundColor:'white',image:IPHONE5? IMAGE_PATH+'tour/13-568h@2x.png' : IMAGE_PATH+'tour/13.png' });
-	var view3 = Ti.UI.createImageView({backgroundColor:'white',image:IPHONE5? IMAGE_PATH+'tour/14-568h@2x.png' : IMAGE_PATH+'tour/14.png' });
-	var view4 = Ti.UI.createImageView({backgroundColor:'white',image:IPHONE5? IMAGE_PATH+'tour/15-568h@2x.png' : IMAGE_PATH+'tour/15.png' });
-	var view5 = Ti.UI.createImageView({backgroundColor:'white',image:IPHONE5? IMAGE_PATH+'tour/16-568h@2x.png' : IMAGE_PATH+'tour/16.png' });
-	var view6 = Ti.UI.createImageView({backgroundColor:'white',image:IPHONE5? IMAGE_PATH+'tour/17-568h@2x.png' : IMAGE_PATH+'tour/17.png' });
+	var view1 = Ti.UI.createImageView({backgroundColor:'white',image:IPHONE5? IMAGE_PATH+'tour/1-568h@2x.png' : IMAGE_PATH+'tour/1.png' });
+	var view2 = Ti.UI.createImageView({backgroundColor:'white',image:IPHONE5? IMAGE_PATH+'tour/2-568h@2x.png' : IMAGE_PATH+'tour/2.png' });
+	var view3 = Ti.UI.createImageView({backgroundColor:'white',image:IPHONE5? IMAGE_PATH+'tour/3-568h@2x.png' : IMAGE_PATH+'tour/3.png' });
+	var view4 = Ti.UI.createImageView({backgroundColor:'white',image:IPHONE5? IMAGE_PATH+'tour/4-568h@2x.png' : IMAGE_PATH+'tour/4.png' });
+	var view5 = Ti.UI.createImageView({backgroundColor:'white',image:IPHONE5? IMAGE_PATH+'tour/5-568h@2x.png' : IMAGE_PATH+'tour/5.png' });
+	var view6 = Ti.UI.createImageView({backgroundColor:'white',image:IPHONE5? IMAGE_PATH+'tour/6-568h@2x.png' : IMAGE_PATH+'tour/6.png' });
+	var view7 = Ti.UI.createImageView({backgroundColor:'white',image:IPHONE5? IMAGE_PATH+'tour/7-568h@2x.png' : IMAGE_PATH+'tour/7.png' });
 	
 	var scrollableView = Ti.UI.createScrollableView({
-	  views:[view1,view2,view3,view4,view5,view6],
+	  views:[view1,view2,view3,view4,view5,view6,view7],
 	  showPagingControl:true
 	});
 	
